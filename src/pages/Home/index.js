59b0480a92b670ab0482d26b10bb7ed78546fc23
GeffersonST <geffsevero@gmail.com>
2021-09-22 23:38:02 -0300

    Usando a API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   constructor(props) {
@@ -13,9 +14,30 @@ class Home extends Component {
   }
   loadFilmes() {
     // URL da API https://sujeitoprogramador.com/r-api/?api=filmes/
+    let url = "https://sujeitoprogramador.com/r-api/?api=filmes";
+    fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        this.setState({ filmes: json });
+        console.log(json);
+      });
   }
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <div className="lista-filmes">
+          {this.state.filmes.map((filme) => {
+            return (
+              <article key={filme.id} className="filme">
+                <strong>{filme.nome}</strong>
+                <img src={filme.foto} alt="Capa" />
+                <Link to="/">Acessar</Link>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
 export default Home;
