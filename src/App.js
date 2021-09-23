13586e5cf369070420edd9567116f80f2c02a4e5
GeffersonST <geffsevero@gmail.com>
2021-09-22 23:03:17 -0300

    Inserindo Consumo de API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-
+import "./style.css";
+import Routes from "./routes";
+// https://sujeitoprogramador.com/r-api/?api=filmes/
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="app">
+        <Routes />
+      </div>
+    );
   }
 }
 export default App;
