784bd7858c50c94f031c61aa231c4cdc80e81424
GeffersonST <geffsevero@gmail.com>
2021-09-23 10:56:14 -0300

    Finalizando

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./filme-info.css";
 class Filme extends Component {
   constructor(props) {
     super(props);
@@ -19,10 +20,10 @@ class Filme extends Component {
 
   render() {
     return (
-      <div>
+      <div className="filme-info">
         <h1> {this.state.filme.nome}</h1>
         <img src={this.state.filme.foto} alt="" />
-        <h3>Sinopse</h3>
+        {this.state.filme.length !== 0 && <h3>Sinopse</h3>}
         {this.state.filme.sinopse}
       </div>
     );
